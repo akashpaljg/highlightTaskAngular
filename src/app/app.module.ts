8c47a0ed67ad1d2017ec752a558a0c42c504b088
akashpaljg <akash.pal@zeuslearning.com>
2024-08-27 19:15:32 +0530

    Added Full code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HighlightComponent } from './highlight/highlight.component';
 import { HighLightModule } from './highlight/highlight.module';
-import { PreviewComponent } from './preview/preview.component';
 import { PreviewModule } from './preview/preview.module';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    PreviewModule,
     HighLightModule,
+    PreviewModule,
     AppRoutingModule
   ],
   providers: [],
