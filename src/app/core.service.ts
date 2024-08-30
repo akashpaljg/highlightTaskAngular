c2700bf69ddb37303e373c41e08bcaa2dd529591
akashpaljg <akash.pal@zeuslearning.com>
2024-08-30 12:02:08 +0530

    Added Comments in Services

--- src/app/core.service.ts
+++ src/app/core.service.ts
@@ -19,7 +19,7 @@ export class CoreService {
 
   /**
    * This will set the concate words
-   * @param value stating the word type, now it is unknownfor the word used fo =r security purpose
+   * @param {boolean} value - stating the word type, now it is unknownfor the word used fo =r security purpose
    */
   setConcateWords(value:boolean){
     this.concateWord.next(value);
@@ -27,67 +27,117 @@ export class CoreService {
 
   /**
    * This will get the concate words
-   * @returns the observable boolean
+   * @returns {boolean} - the observable boolean
    */
   getConcateWords():Observable<boolean>{
     return this.concateWord;
   }
 
-  // Setting visibility of selector and correct answers
+  /**
+   * This will set the visibility in selector and correct section
+   * @param {boolean} value - it is the value of visible
+   */
   setVisibility(value:boolean){
     this.isVisible.next(value);
   }
 
+  /**
+   * This will get the visibility in selector and correct section
+   * @returns {Observable<boolean>}  the value of visibility
+   */
   getVisibility():Observable<boolean>{
     return this.isVisible;
   }
 
-  // Setting complete question
+  /**
+   * Sets the complete question by emitting the provided question object.
+   * This updates the observable that tracks the current complete question.
+   *
+   * @param {IQuestion} question - The question object to be set. 
+   *                                It should contain all necessary details such as the question text, options, etc.
+   */
   setCompleteQuestion(question: IQuestion) {
     this.completeQuestion.next(question);
     console.log('Complete Question set');
   }
 
   getCompleteQuestion(): Observable<IQuestion | null>  {
-    return this.completeQuestion.asObservable();
+      return this.completeQuestion.asObservable();
   }
 
-  // Select Validator
+    /**
+   * Sets the validation state for the select input.
+   *
+   * @param {boolean} value - A boolean value indicating whether the select input is valid or not.
+   */
   setValidateSelect(value: boolean) {
     this.validateSelect.next(value);
   }
 
+  /**
+   * Returns an observable that emits the current validation state of the select input.
+   *
+   * @returns {Observable<boolean>} - An observable of the select input's validation state.
+   */
   getValidateSelect(): Observable<boolean> {
-    return this.validateSelect.asObservable()
+    return this.validateSelect.asObservable();
   }
 
-  // Highlight save and preview section
+  /**
+   * Sets the preview state for the highlight section.
+   *
+   * @param {boolean} value - A boolean value indicating whether the preview is active or not.
+   */
   setIsPreview(value: boolean) {
     this.isPreview.next(value);
   }
 
+  /**
+   * Returns an observable that emits the current preview state of the highlight section.
+   *
+   * @returns {Observable<boolean>} - An observable of the preview state.
+   */
   getIsPreview(): Observable<boolean> {
     return this.isPreview.asObservable();
   }
 
-  // Playing with options
+  /**
+   * Sets the available options for selection.
+   *
+   * @param {IOptions[]} value - An array of IOptions objects representing the options.
+   */
   setOptions(value: IOptions[]) {
     this.options.next(value);
     this.editOptions();
   }
 
+  /**
+   * Returns an observable that emits the current list of options available for selection.
+   *
+   * @returns {Observable<IOptions[]>} - An observable of the options array.
+   */
   getOptions(): Observable<IOptions[]> {
     return this.options.asObservable();
   }
 
-  // Total Correct Answers
+  /**
+   * Sets the total number of correct answers.
+   *
+   * @param {number[]} value - An array of numbers representing the indices of correct answers.
+   */
   setTotalCorrectAnswer(value: number[]) {
     this.totalCorrectAnswers.next(value);
   }
 
+  /**
+   * Returns an observable that emits the current list of correct answers' indices.
+   *
+   * @returns {Observable<number[]>} - An observable of the correct answers' indices.
+   */
   getTotalCorrectAnswer(): Observable<number[]> {
     return this.totalCorrectAnswers.asObservable();
   }
+
 
   editOptions() {
     const currentOptions = this.options.getValue();
